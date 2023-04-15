f4f6f6eccc1db9e3ad30d4b419fd8ee76c7f5f7b
Mikkel <58807701+professorcrazy@users.noreply.github.com>
2023-04-15 23:23:33 +0200

    Update EndScoreManager.cs

--- Assets/EndScoreManager.cs
+++ Assets/EndScoreManager.cs
@@ -6,6 +6,10 @@ public class EndScoreManager : MonoBehaviour
     [SerializeField] GameObject[] scoreImages;
     [SerializeField] GameObject restartButton;
     // Start is called before the first frame update
+    public EndScoreManager Factory(Transform parent) {
+        var instance = Instantiate(this, parent);
+        return instance;
+    }
     void Start()
     {
         foreach (GameObject image in scoreImages) {
