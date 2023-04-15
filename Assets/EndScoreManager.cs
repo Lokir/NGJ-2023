7fc98637bb581f9a3347ecd8c883cda7e08c1d9f
Mikkel <58807701+professorcrazy@users.noreply.github.com>
2023-04-15 23:15:34 +0200

    Winscreen and nasty bread implentation

---
+++ Assets/EndScoreManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScoreManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] scoreImages;
+    [SerializeField] GameObject restartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject image in scoreImages) {
+            image.SetActive(false);
+            restartButton.SetActive(false);
+        }
+    }
+    public void GameOver(int score) {
+        if (score >= scoreImages.Length) {
+            score = scoreImages.Length - 1;
+        }
+        if (score < 0 ) {
+            score = 0;
+        }
+        scoreImages[score].SetActive(true);
+        restartButton.SetActive(true);
+    }
+
+}
