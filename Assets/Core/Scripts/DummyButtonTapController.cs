a34ce247cc0858cb99beaad2eee474448a8fa878
Loki <Pdhh@live.dk>
2023-04-16 02:05:30 +0200

    w.i.p.

--- Assets/Core/Scripts/DummyButtonTapController.cs
+++ Assets/Core/Scripts/DummyButtonTapController.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
 
 namespace Core.Scripts
 {
     public class DummyButtonTapController : MonoBehaviour, IButtonTapController
     {
-        
+        public void SubscribeToSpinDirectionEvent(Action<IQuickTimeTapButtonEventPayload> actionToSubscribe)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Assets/Core/Scripts/IButtonTapController.cs
+++ Assets/Core/Scripts/IButtonTapController.cs
@@ -2,6 +2,6 @@ namespace Core.Scripts
 {
     public interface IButtonTapController 
     {
-   
+        void SubscribeToSpinDirectionEvent(Action<IQuickTimeTapButtonEventPayload> actionToSubscribe);
     }
 }
